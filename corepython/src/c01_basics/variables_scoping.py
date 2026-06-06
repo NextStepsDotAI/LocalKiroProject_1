3254fa056d57556e097eb5ba28420672f2bc19ed
NextSteps.AI <224296288+NextStepsDotAI@users.noreply.github.com>
2026-06-06 19:52:08 +0530

    Dunder variables fixing bug

--- corepython/src/c01_basics/variables_scoping.py
+++ corepython/src/c01_basics/variables_scoping.py
@@ -8,7 +8,6 @@ def read_global():
     # Reading a global variable is freely allowed from anywhere
     print(f"Reading global variable: {my_global}")
 
-
 def modify_global_with_keyword():
     # To modify the global variable, we MUST declare it explicitly
     global my_global
