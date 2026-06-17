f45e8f2684c222be1cd1bd7eed93735ffeea473d
NextStepsDotAI <224296288+NextStepsDotAI@users.noreply.github.com>
2026-06-17 10:46:54 +0530

    Add c01_basics: hello_world.py exercise template

---
+++ corepython/src/c01_basics/hello_world.py
@@ -0,0 +1,14 @@
+# Exercise: Hello World variations
+# Complete the TODO items below
+
+def greet_simple():
+    """TODO: Make this function print 'Hello, World!'"""
+    pass
+
+def greet_with_name(name):
+    """TODO: Make this function print 'Hello, [name]!'"""
+    pass
+
+if __name__ == "__main__":
+    greet_simple()
+    greet_with_name("Python Learner")
